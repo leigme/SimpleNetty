7a0e48753c0b35735f041ea8f005ddca31229809
leigme <leigme@gmail.com>
2018-05-02 23:20:49 +0800

    修改格式

--- src/main/java/me/leig/simplenetty/App.java
+++ src/main/java/me/leig/simplenetty/App.java
@@ -4,10 +4,8 @@ package me.leig.simplenetty;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
